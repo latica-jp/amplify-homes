78e9413b4e1d895bb8276b99c90183d2f4761bb6
latica-jp <isao@sonicgaden.jp>
2022-01-26 10:00:51 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import { MarketingFooter, NavBar, NewHomes } from './ui-components';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <NewHomes isPaginated itemsPerPage={3} />
+      <MarketingFooter />
     </div>
   );
 }
